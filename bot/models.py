963155702ef32616019246e797ce990317611043
Kisly93 <spsvk.kis.sov@gmail.com>
2023-06-21 17:45:05 +0200

    Обновил модели

--- bot/models.py
+++ bot/models.py
@@ -1,6 +1,4 @@
-from django.core.exceptions import ValidationError
 from django.db import models
-import datetime
 
 
 class Member(models.Model):
@@ -9,7 +7,8 @@ class Member(models.Model):
                                null=True, blank=True)
     name = models.CharField(max_length=40, verbose_name='Имя участника',
                             null=True, blank=True)
-    hi_speaker = models.BooleanField(default=False, verbose_name='Докладчик',)
+    is_speaker = models.BooleanField(default=False, verbose_name='Докладчик', )
+
     class Meta:
         verbose_name = 'Участник'
         verbose_name_plural = 'Участники'
@@ -37,17 +36,36 @@ class Report(models.Model):
 class Question(models.Model):
     title = models.TextField(max_length=200, verbose_name='Вопрос',
                              null=True, blank=True)
-    asking = models.ForeignKey(Member, on_delete=models.CASCADE,
-                               verbose_name='Спрашивающий',
-                               related_name='my_questions')
-    responsible = models.ForeignKey(Member, on_delete=models.CASCADE,
-                                    verbose_name='Отвечающий',
-                                    related_name='me_questions')
-    published_at = models.DateTimeField("Дата и время")
-    status = models.BooleanField(default=True, verbose_name='Ответили на вопрос',)
+    asker = models.ForeignKey(Member, on_delete=models.CASCADE,
+                              verbose_name='Задавший вопрос',
+                              related_name='asked_questions',
+                              null=True, blank=True)
+    responder = models.ForeignKey(Member, on_delete=models.CASCADE,
+                                  verbose_name='Отвечающий',
+                                  related_name='answered_questions')
+    report = models.ForeignKey(Report, on_delete=models.CASCADE, verbose_name='Презентация',
+                               related_name='questions',
+                               null=True, blank=True)
+
+    is_answered = models.BooleanField(default=False, verbose_name='Вопрос отвечен')
+
     class Meta:
         verbose_name = 'Вопрос'
         verbose_name_plural = 'Вопросы'
 
     def __str__(self):
         return f'#{self.pk} {self.title}'
+
+
+class Event(models.Model):
+    date = models.DateField(verbose_name='Дата мероприятия')
+    location = models.CharField(max_length=100, verbose_name='Место проведения')
+    speakers = models.ManyToManyField(Member, verbose_name='Список спикеров', related_name='events')
+    program = models.TextField(verbose_name='Программа мероприятия')
+
+    class Meta:
+        verbose_name = 'Мероприятие'
+        verbose_name_plural = 'Мероприятия'
+
+    def __str__(self):
+        return f'#{self.pk} {self.date}'
